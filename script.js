49853403576fc91ab2193e46d17544ed92571285
brixdesalit7 <brixdesalit7@gmail.com>
2022-11-04 10:08:27 +0800

    first upload

--- script.js
+++ script.js
@@ -2,21 +2,6 @@ let quotesData;
 var currentQuote = '',
 currentAuthor = '';
 
-var colors = [
-    '#16a085',
-    '#27ae60',
-    '#2c3e50',
-    '#f39c12',
-    '#e74c3c',
-    '#9b59b6',
-    '#FB6964',
-    '#342224',
-    '#472E32',
-    '#BDBB99',
-    '#77B1A9',
-    '#73A857'
-  ];
-
 const getQuotes = () => {
 return $.ajax({
     headers: {
@@ -30,8 +15,7 @@ return $.ajax({
     }
 });
 }
-
-const getRandomQuote = () => {
+const getRandomQuote = () => {   
     return quotesData.quotes[ Math.floor(Math.random() * quotesData.quotes.length)];
 }
 
@@ -57,21 +41,9 @@ const getQuote = () => {
         $('#author').html(randomQuote.author);
     });
 
-    
-    var color = Math.floor(Math.random() * colors.length);
-    $('html body').animate(
-        {
-        backgroundColor: colors[color],
-        color: colors[color]
-        },
-        1000
-    );
-    $('.button').animate(
-        {
-        backgroundColor: colors[color]
-        },
-        1000
-    );
+    $('.button').animate({opacity: 0}, 500, function() {
+        $(this).animate({ opacity: 1}, 500);
+    });
 }
 
 $(document).ready(function () {
